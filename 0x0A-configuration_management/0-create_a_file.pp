fe551581c02cf7a8e669d6485fc59219f90e0cea
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-25 19:59:29 +0300

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file in /tmp
+file { '/tmp/school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
