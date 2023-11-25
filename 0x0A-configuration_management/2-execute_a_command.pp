8b704b21fc4d0e5806fed7f0ea8905189a19f3af
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-25 20:38:00 +0300

    install flask

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 # creates a manifest that kills a process named killmenow.
 exec { 'killmenow':
-  command => 'pkill killmenow',
+  command => 'pkill -f killmenow',
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
