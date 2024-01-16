cae2c4c1d0d713cae7bc8908b80903aa5ba91ec7
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-16 14:14:25 +0300

    requirement fail. removed import alias

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit"""
-import requests as req
+import requests
 
 
 def number_of_subscribers(subreddit):
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     header = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:121.0)"
     }
-    resp = req.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+    resp = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
                    headers=header, allow_redirects=False)
     if resp.status_code == 404 or\
             'subscribers' not in resp.json()['data'].keys():
