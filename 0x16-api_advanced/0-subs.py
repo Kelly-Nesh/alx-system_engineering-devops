61f5441b6681d76a4e944df9abbe8ce84594669c
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-16 14:21:43 +0300

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:121.0)"
     }
     resp = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                   headers=header, allow_redirects=False)
+                        headers=header, allow_redirects=False)
     if resp.status_code == 404:
         return 0
     try:
