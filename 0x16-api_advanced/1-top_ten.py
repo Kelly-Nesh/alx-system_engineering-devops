dd4a38c5261df2fce17bbb2d5804b0939e3f733e
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-17 23:54:30 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,8 +5,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10
-hot posts listed."""
+    """prints the titles of the first 10 hot posts listed."""
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
@@ -19,3 +18,24 @@ hot posts listed."""
         return
     data = response.json().get("data")
     [print(c.get("data").get("title")) for c in data.get("children")]
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+            'User-Agent': 'Mozilla/5.0'
+    }
+    params = {
+            'limit': 10
+    }
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    res = response.json()
+    hot_posts = res['data']['children']
+    if len(hot_posts) == 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
