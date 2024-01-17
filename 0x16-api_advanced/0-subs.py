c49e7cefed40093bbc7f040da0164a6b8e73269c
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-17 23:51:16 +0300

    0-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,20 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    header = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:121.0)"
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
     }
-    resp = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                        headers=header, allow_redirects=False)
-    if resp.status_code == 404:
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
-    try:
-        return resp.json().get("data").get("subscribers")
-    except requests.exceptions.JSONDecodeError:
+    dic = res.json()
+    if 'data' not in dic:
         return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
